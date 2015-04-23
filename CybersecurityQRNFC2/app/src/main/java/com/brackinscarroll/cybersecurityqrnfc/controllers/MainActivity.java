bbdccb476589515332c3f0bbe3f6fccdb63f13c5
Samuel Carroll <samuel.carroll@mines.sdsmt.edu>
2015-04-23 14:04:13 -0600

    Initial RSA encode and Decode
    Right now I take an EditText and the 'message' in there is run through
    RSA with new generating keys every time. Will need to write the KeyPair
    to memory and then check if they are there or generate once and use a
    shared preference to say I have a keypair stored (probably faster).

--- CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/controllers/MainActivity.java
+++ CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/controllers/MainActivity.java
@@ -5,12 +5,20 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.os.Build;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.security.KeyPairGenerator;
+import java.security.KeyPair;
+import java.security.Key;
+import javax.crypto.Cipher;
 
 import com.brackinscarroll.cybersecurityqrnfc.R;
 import com.brackinscarroll.cybersecurityqrnfc.interfaces.MainFragmentListener;
@@ -72,5 +80,75 @@ public class MainActivity extends ActionBarActivity implements MainFragmentListe
     {
         super.onSaveInstanceState( bundle );
         //Put stuff into the bundle
+    }
+
+    @Override
+    public void onButtonEncodeClicked(View view)
+    {
+        TextView TextViewMessage = (TextView) findViewById(R.id.editText_Message);
+
+        Key publicKey = null;
+        Key privateKey = null;
+
+        //if no key pair found in memory call function to end of catch
+        try
+        {
+            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+            kpg.initialize(1024);
+            KeyPair kp = kpg.genKeyPair();
+            publicKey = kp.getPublic();
+            privateKey = kp.getPrivate();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Generate Key Pair", Toast.LENGTH_SHORT).show();
+        }
+        // else get keypair from memory
+
+        // call when we send the message, send with public key
+        byte[] encodedBytes = null;
+        try
+        {
+            Cipher c = Cipher.getInstance("RSA");
+            c.init(Cipher.ENCRYPT_MODE, privateKey);
+            encodedBytes = c.doFinal(TextViewMessage.getText().toString().getBytes());
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Encrypt with RSA", Toast.LENGTH_SHORT).show();
+        }
+        // send encoded bytes
+
+        TextView TextViewEncoded = (TextView)findViewById(R.id.textView_EncodedMessage);
+        try
+        {
+            TextViewEncoded.setText("Encoded Message: " + Base64.encodeToString(encodedBytes, Base64.DEFAULT));
+        }
+        catch (Exception ex)
+        {
+            TextViewEncoded.setText("Encoded Message: ");
+        }
+
+        byte[] decodedBytes = null;
+        try
+        {
+            Cipher c = Cipher.getInstance("RSA");
+            c.init(Cipher.DECRYPT_MODE, publicKey);
+            decodedBytes = c.doFinal(encodedBytes);
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Encrypt with RSA", Toast.LENGTH_SHORT).show();
+        }
+
+        TextView TextViewDecoded = (TextView)findViewById(R.id.textView_DecodedMessage);
+        try
+        {
+            TextViewDecoded.setText("Decoded Message: " + new String(decodedBytes));
+        }
+        catch (Exception ex)
+        {
+            TextViewDecoded.setText("Decoded Message: ");
+        }
     }
 }
--- CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/interfaces/MainFragmentListener.java
+++ CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/interfaces/MainFragmentListener.java
@@ -1,8 +1,11 @@
 package com.brackinscarroll.cybersecurityqrnfc.interfaces;
 
+import android.view.View;
+
 /**
  * Created by Julian on 4/12/2015.
  */
 public interface MainFragmentListener
 {
+    public void onButtonEncodeClicked (View view);
 }
