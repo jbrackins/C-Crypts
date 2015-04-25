3f40f1e9d399324abfba36e930cb2980b20a780e
Samuel Carroll <samuel.carroll@mines.sdsmt.edu>
2015-04-25 17:09:48 -0600

    Cleaned up RSA
    Cleaned up RSA Code, separated into functions, made a shared preferences
    that says we have saved the Keys and saved the keys to system memory.

--- CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/controllers/MainActivity.java
+++ CybersecurityQRNFC2/app/src/main/java/com/brackinscarroll/cybersecurityqrnfc/controllers/MainActivity.java
@@ -1,24 +1,26 @@
 package com.brackinscarroll.cybersecurityqrnfc.controllers;
 
 import android.app.FragmentManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Base64;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.security.KeyPairGenerator;
 import java.security.KeyPair;
 import java.security.Key;
 import javax.crypto.Cipher;
+import java.security.KeyFactory;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
 
 import com.brackinscarroll.cybersecurityqrnfc.R;
 import com.brackinscarroll.cybersecurityqrnfc.interfaces.MainFragmentListener;
@@ -27,6 +29,16 @@ import com.brackinscarroll.cybersecurityqrnfc.views.MainFragment;
 
 public class MainActivity extends ActionBarActivity implements MainFragmentListener
 {
+    private KeyPair m_keyPair = null;
+    private Key m_publicKey = null;
+    private Key m_privateKey = null;
+    private boolean m_keyPairGenerated = false;
+
+    private final String m_publicFileName = "public.key";
+    private final String m_privateFileName = "private.key";
+    private final int m_keyLength = 1024;
+
+    public static final String KEY_PAIR_GENERATED = "KeyPairFile";
 
     private MainFragment _mainFragment;
     @Override
@@ -46,6 +58,28 @@ public class MainActivity extends ActionBarActivity implements MainFragmentListe
         else
         {
 
+        }
+
+        SharedPreferences settings = getSharedPreferences(KEY_PAIR_GENERATED, MODE_PRIVATE);
+        m_keyPairGenerated = settings.getBoolean("keyPairGenerated", false);
+
+        if (m_keyPairGenerated)
+        {
+            readKeys(getApplicationContext());
+        }
+    }
+
+    @Override
+    protected void onResume ( )
+    {
+        super.onResume();
+
+        SharedPreferences settings = getSharedPreferences(KEY_PAIR_GENERATED, MODE_PRIVATE);
+        m_keyPairGenerated = settings.getBoolean("keyPairGenerated", false);
+
+        if (m_keyPairGenerated && m_privateKey == null && m_publicKey == null)
+        {
+            readKeys(getApplicationContext());
         }
     }
 
@@ -87,68 +121,214 @@ public class MainActivity extends ActionBarActivity implements MainFragmentListe
     {
         TextView TextViewMessage = (TextView) findViewById(R.id.editText_Message);
 
-        Key publicKey = null;
-        Key privateKey = null;
-
         //if no key pair found in memory call function to end of catch
-        try
-        {
-            KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
-            kpg.initialize(1024);
-            KeyPair kp = kpg.genKeyPair();
-            publicKey = kp.getPublic();
-            privateKey = kp.getPrivate();
-        }
-        catch (Exception ex)
-        {
-            Toast.makeText(this, "Couldn't Generate Key Pair", Toast.LENGTH_SHORT).show();
-        }
+
         // else get keypair from memory
 
-        // call when we send the message, send with public key
-        byte[] encodedBytes = null;
-        try
+        if (getKeys())
         {
-            Cipher c = Cipher.getInstance("RSA");
-            c.init(Cipher.ENCRYPT_MODE, privateKey);
-            encodedBytes = c.doFinal(TextViewMessage.getText().toString().getBytes());
-        }
-        catch (Exception ex)
-        {
-            Toast.makeText(this, "Couldn't Encrypt with RSA", Toast.LENGTH_SHORT).show();
-        }
-        // send encoded bytes
+            byte[] encodedBytes = encodeMessage(TextViewMessage.getText().toString().getBytes());
 
-        TextView TextViewEncoded = (TextView)findViewById(R.id.textView_EncodedMessage);
-        try
-        {
-            TextViewEncoded.setText("Encoded Message: " + Base64.encodeToString(encodedBytes, Base64.DEFAULT));
-        }
-        catch (Exception ex)
-        {
-            TextViewEncoded.setText("Encoded Message: ");
-        }
+            // send encoded bytes
 
+            TextView TextViewEncoded = (TextView) findViewById(R.id.textView_EncodedMessage);
+            try
+            {
+                TextViewEncoded.setText("Encoded Message: " + Base64.encodeToString(encodedBytes, Base64.DEFAULT));
+            }
+            catch (Exception ex)
+            {
+                TextViewEncoded.setText("Encoded Message: ");
+            }
+
+            byte[] decodedBytes = decodeMessage(encodedBytes);
+
+            TextView TextViewDecoded = (TextView) findViewById(R.id.textView_DecodedMessage);
+            try
+            {
+                TextViewDecoded.setText("Decoded Message: " + new String(decodedBytes));
+            }
+            catch (Exception ex)
+            {
+                TextViewDecoded.setText("Decoded Message: ");
+            }
+        }
+    }
+
+    private byte[] decodeMessage(byte[] encodedMessage)
+    {
         byte[] decodedBytes = null;
         try
         {
             Cipher c = Cipher.getInstance("RSA");
-            c.init(Cipher.DECRYPT_MODE, publicKey);
-            decodedBytes = c.doFinal(encodedBytes);
+            c.init(Cipher.DECRYPT_MODE, m_publicKey);
+            decodedBytes = c.doFinal(encodedMessage);
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Decrypt with RSA", Toast.LENGTH_SHORT).show();
+        }
+
+        return decodedBytes;
+    }
+
+    private byte[] encodeMessage(byte[] plainMessage)
+    {
+        byte[] encodedBytes = null;
+
+        try
+        {
+            Cipher c = Cipher.getInstance("RSA");
+            c.init(Cipher.ENCRYPT_MODE, m_privateKey);
+            encodedBytes = c.doFinal(plainMessage);
+
+//            encodedBytes = c.doFinal(TextViewMessage.getText().toString().getBytes());
         }
         catch (Exception ex)
         {
             Toast.makeText(this, "Couldn't Encrypt with RSA", Toast.LENGTH_SHORT).show();
         }
 
-        TextView TextViewDecoded = (TextView)findViewById(R.id.textView_DecodedMessage);
+        return encodedBytes;
+    }
+
+    private boolean getKeys()
+    {
+        if (m_keyPairGenerated)
+        {
+            if (m_publicKey == null || m_privateKey == null)
+            {
+                readKeys(getApplicationContext());
+            }
+
+            return true;
+        }
+        else
+        {
+            if (m_publicKey == null || m_privateKey == null)
+            {
+                try
+                {
+                    KeyPairGenerator kpg = KeyPairGenerator.getInstance("RSA");
+                    kpg.initialize(m_keyLength);
+                    m_keyPair = kpg.genKeyPair();
+                    m_publicKey = m_keyPair.getPublic();
+                    m_privateKey = m_keyPair.getPrivate();
+
+                    m_keyPairGenerated = true;
+
+                    writeKeys(getApplicationContext());
+
+                    return true;
+                }
+                catch (Exception ex)
+                {
+                    Toast.makeText(this, "Couldn't Generate Key Pair", Toast.LENGTH_SHORT).show();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    private Boolean readKeys(Context context)
+    {
+        byte [] publicBytes;
+        byte [] privateBytes;
+
+        KeyFactory keyFactory;
+
         try
         {
-            TextViewDecoded.setText("Decoded Message: " + new String(decodedBytes));
+            keyFactory = KeyFactory.getInstance("RSA");
         }
         catch (Exception ex)
         {
-            TextViewDecoded.setText("Decoded Message: ");
+            Toast.makeText(this, "Couldn't get instance of key factory", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        try
+        {
+            FileInputStream fis = context.openFileInput(m_publicFileName);
+            publicBytes = new byte[m_keyLength];
+            fis.read(publicBytes);
+            fis.close();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Open Public Input File" + ex, Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        try
+        {
+            FileInputStream fis = openFileInput(m_privateFileName);
+            privateBytes = new byte[m_keyLength];
+            fis.read(privateBytes);
+            fis.close();
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't Open Private Input File", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        try
+        {
+            X509EncodedKeySpec encodedPubSpec = new X509EncodedKeySpec(publicBytes);
+            m_publicKey = keyFactory.generatePublic(encodedPubSpec);
+
+            PKCS8EncodedKeySpec encodedPrivSpec = new PKCS8EncodedKeySpec(privateBytes);
+            m_privateKey = keyFactory.generatePrivate(encodedPrivSpec);
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+    }
+
+    private void writeKeys(Context context)
+    {
+        Boolean publicWritten = false;
+        Boolean privateWritten = false;
+
+        try
+        {
+            X509EncodedKeySpec encodedPubSpec = new X509EncodedKeySpec(m_publicKey.getEncoded());
+
+            FileOutputStream fos = context.openFileOutput(m_publicFileName, Context.MODE_PRIVATE);
+            fos.write(encodedPubSpec.getEncoded());
+            fos.close();
+
+            publicWritten = true;
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't open output file", Toast.LENGTH_SHORT).show();
+        }
+        try
+        {
+            PKCS8EncodedKeySpec encodedPrivSpec = new PKCS8EncodedKeySpec(m_privateKey.getEncoded());
+
+            FileOutputStream fos = openFileOutput(m_privateFileName, Context.MODE_PRIVATE);
+            fos.write(encodedPrivSpec.getEncoded());
+            fos.close();
+
+            privateWritten = true;
+        }
+        catch (Exception ex)
+        {
+            Toast.makeText(this, "Couldn't open private output file", Toast.LENGTH_SHORT).show();
+        }
+
+        if (publicWritten && privateWritten)
+        {
+            SharedPreferences settings = getSharedPreferences(KEY_PAIR_GENERATED, MODE_PRIVATE);
+            SharedPreferences.Editor editor = settings.edit();
+            editor.putBoolean("keyPairGenerated", true);
+            editor.commit();
         }
     }
 }
